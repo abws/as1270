455edf86e6c9379cc172c7e7d92ec9e60b759c38
A Salah <as1270@student.le.ac.uk>
2023-01-06 22:27:19 +0000

    Implement roulette wheel selection
    - very fun stuff
    - i had an inspiration today, an enlightening if i can put it that way
    - ill create a short 'novel story' or adventure with my research side of this project
    - this will include trying out new algorithms, playing around with the parameters and comparing them
    - on the other side, ill carry on with creating my web-based tool
    - this way i can get the best of both worlds
    -but then again im probably trapped in the planning fallacy.
    - who knows if i have enough time
    -well, theres only one way to find out...

--- semester_1/algorithms/SimpleGeneticAlgorithm.java
+++ semester_1/algorithms/SimpleGeneticAlgorithm.java
@@ -7,19 +7,22 @@ import semester_1.problems.ProblemConstrainedQuadratic;
 /**
  * Simple Genetic Algorithm
  * Uses roulette selection and one point crossover,
- * and a random mutation rate 0.2.
- * Population size is set at 4, and crossover happens at rate 1
+ * and a random mutation rate 0.25.
+ * Population size is set at 4, and crossover happens at rate 0.7
  * @author Abdiwahab Salah
  * @version 05.01.23
  */
 public class SimpleGeneticAlgorithm {
-    private final int POP_SIZE = 4;
+    private static final int POP_SIZE = 4;
+    private static final double MUT_RATE = 1 / POP_SIZE;
+    privatestatic  final double CROSSOVER_RATE = 0.7;
 
     public static int run(ProblemConstrainedQuadratic problem) {
         ArrayList<Individual> population = problem.getRandomPopulation(4, 4); //fitness is calculated with the birth of an individual
         ArrayList<Integer> weights = calculateWeights(population);
+        ArrayList<Individual> offsprings = new ArrayList<>();
+        ArrayList<Individual> matingPool = reproduce(population, weights, POP_SIZE);
 
-        ArrayList<Individual> matingPool = reproduce(POP_SIZE);
         newPopulation = matingPool.onePointCrossOver(POP_SIZE / 2);
 
         for (Individual i : population) {
@@ -29,9 +32,34 @@ public class SimpleGeneticAlgorithm {
         return 0;
     }
 
-    private int rouletteSelection(){};
-    private int reproduce(){};
-    private int onePointCrossOver(){};
+    private int reproduce(ArrayList<Individual> population, ArrayList<Integer> weights, int populationSize) {
+        ArrayList<Individual> matingPool = rouletteSelection(population, weights, POP_SIZE);
+        onePointCrossover(matingPool, CROSSOVER_RATE);
+
+
+    }
+
+
+    private ArrayList<Individual> rouletteSelection(ArrayList<Individual> population, ArrayList<Integer> weights, int populationSize) {
+        double randomSpin = Math.random();  //This generated a random number from 0-1 (similar to a random spin)
+        double cumulativeWeight = 0;        //Represents the starting position of the roulette wheel
+
+        ArrayList<Individual> matingPool = new ArrayList<>();
+
+        for (int n = 0; n < populationSize; n++) {
+            for (int i = 0; i < weights.size(); i++) {  //Stack up each weight. The space it takes up from 0-1 corresponds to how large it is (i.e. its weight)
+                cumulativeWeight += weights.get(i);
+
+                if (randomSpin <= cumulativeWeight) {
+                    matingPool.add(population.get(i));
+                }
+            }
+        }
+
+        return matingPool;
+    }
+
+    private int onePointCrossOver(){}
 
     private static ArrayList<Integer> calculateWeights(ArrayList<Individual> population) {
         int sum = 0;
