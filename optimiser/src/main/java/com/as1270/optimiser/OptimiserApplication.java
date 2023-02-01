7d93b5ee8a013b955d657166ce21dcb0d831d713
A Salah <as1270@student.le.ac.uk>
2023-02-01 21:42:16 +0000

    Create Spring Boot application for software tool
    - this will make up part 2 of my project
    - uses spring boot and maven as dependency management
    good times ahead! keep going

---
+++ optimiser/src/main/java/com/as1270/optimiser/OptimiserApplication.java
@@ -0,0 +1,13 @@
+package com.as1270.optimiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimiserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimiserApplication.class, args);
+    }
+
+}
---
+++ optimiser/src/test/java/com/as1270/optimiser/OptimiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as1270.optimiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimiserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
