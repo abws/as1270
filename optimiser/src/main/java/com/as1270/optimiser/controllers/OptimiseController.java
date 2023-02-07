d5c574e4daf23f7c76bc0b95a2b2d32e2f9fa181
A Salah <as1270@student.le.ac.uk>
2023-02-07 21:31:46 +0000

    Develop basic html page for optimise page of tool

--- optimiser/src/main/java/com/as1270/optimiser/controllers/OptimiseController.java
+++ optimiser/src/main/java/com/as1270/optimiser/controllers/OptimiseController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class OptimiseController {
 
     @GetMapping("/")
     public String showOptimisePage(Model model) {
-        model.addAttribute("name", "Abdiwahab");
+        //run algorithm, model will contain a very large double[nturbines][2] of coordinates
+        double[][] coordinates = {{5,2}, {3, 4}, {3, 5}, {6, 7}};
+
+        model.addAttribute("name", Arrays.deepToString(coordinates));
         return "optimise";
     }
 }
