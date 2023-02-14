b1f78906698c7aae1204c489ed5d2d45af1d9543
A Salah <as1270@student.le.ac.uk>
2023-02-14 08:15:26 +0000

    Complete Particle book keeping class
    - represents a single particle in the swam

--- research/particleswarmoptimisation/Particle.java
+++ research/particleswarmoptimisation/Particle.java
@@ -2,28 +2,45 @@ package research.particleswarmoptimisation;
 
 /**
  * Class representing 
- * a single particle.
+ * a single particle floating
+ * through the search space.
+ * Only concerned with book-keeping.
+ * No operations should be performed
+ * here.
  */
 public class Particle {
     double[] currentPosition;
     double[] velocity;
+    double[] pBest;
 
-    double pBest;
+    double currentValue;
+    double pBestValue;
     
 
     //A new particle will be created once
-    Particle(double[] currentPosition, Problem problem) { //Problem provides the evaluation function
-        this.currentPosition = currentPosition;
+    Particle(double[] initialPosition, double[] initialVelocity, Problem problem) { //Problem provides the evaluation function
+        this.currentPosition = initialPosition;
+        this.velocity = initialVelocity;
+        updateCurrentValue();
     }
 
-   public void updateCurrentPosition(double[] newPosition) {
-
+   public void updateCurrentPosition(double[] newPosition) {    //PSO class does the calculations
+       this.currentPosition = newPosition;
    }
 
-   
+   public void updateVelocity(double[] newVelocity) {   //PSO class does the calculations
+       this.velocity = newVelocity;
+   }
 
+   public void updateCurrentValue() {
+       double fitness = problem.evaluate(currentPosition);
+       this.currentValue = fitness;
+   }
 
-
-
-    
+   public void updatePersonalBest() {
+       if (currentValue > pBestValue) { //assuming maximisation
+            pBest = currentPosition;
+           pBestValue = currentValue;
+       }
+   }
 }
